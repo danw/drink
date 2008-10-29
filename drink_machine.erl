c6015d1fb33b99f31f30d60811dfaf2266ab908f
Dan Willemsen <dan@csh.rit.edu>
2008-10-29 03:01:12 +0000

    move drink_machine to gen_server

--- drink_machine.erl
+++ drink_machine.erl
@@ -1,97 +1,102 @@
 -module (drink_machine).
+-behaviour (gen_server).
 
 -export ([start_link/2]).
--export ([init/3]).
--export ([system_continue/3, system_terminate/4]).
+-export ([init/1]).
+-export ([handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export ([got_response/2]).
 -export ([drop/2]).
 
 -record (dmstate, {
 			machineid,
 			commpid,
-			temperature}).
+			temperature,
+			droppid,
+			dropref,
+			timerref}).
 
 start_link (MachineId, CommPid) ->
-	proc_lib:start_link(?MODULE, init, [self(), MachineId, CommPid]).
+	gen_server:start_link({local, list_to_atom(lists:append("drink_machine_", atom_to_list(MachineId)))},
+							?MODULE, {MachineId, CommPid}, []).
 
-init (Parent, MachineId, CommPid) ->
-	Name = list_to_atom(lists:append(["drink_machine_", atom_to_list(MachineId)])),
-	register(Name, self()),			% Register as drink_machine_(id)
-	Debug = sys:debug_options([]),
-	proc_lib:init_ack(Parent, {ok, self()}),
-	loop({idle, #dmstate{machineid=MachineId,commpid=CommPid}}, Parent, Debug).
+init ({MachineId, CommPid}) ->
+	{ok, {idle, #dmstate{machineid=MachineId,commpid=CommPid}}}.
 
-loop ({idle, State}, Parent, Debug) ->
-	#dmstate{commpid=CommPid} = State,
-	receive
-		{got_response, CommPid, {temperature, _DateTime, Temperature}} ->
-			io:format("Got temperature: ~p~n", [Temperature]),
-			loop({idle, State#dmstate{temperature=Temperature}}, Parent, Debug);
-		{got_response, CommPid, drop_ack} ->
-			error_logger:error_msg("Drop Ack Received while not dropping!"),
-			loop({idle, State}, Parent, Debug);
-		{got_response, CommPid, drop_nack} ->
-			error_logger:error_msg("Drop Nack Received while not dropping!"),
-			loop({idle, State}, Parent, Debug);
-		{got_response, CommPid, Response} ->
-			io:format("Unknown response: ~p~n", [Response]),
-			loop({idle, State}, Parent, Debug);
-		{timeout, _Tref} ->
-			loop({idle, State}, Parent, Debug);
-		{api, drop, RetPid, DropRef, Slot} ->
-			case timer:send_after(timer:seconds(30), {timeout, DropRef}) of
-				{ok, Timer} ->
-					drink_machine_comm:send_command(CommPid, {drop, Slot}),
-					loop({dropping, RetPid, Timer, DropRef, State}, Parent, Debug);
-				{error, Reason} ->
-					RetPid ! {drop_result, DropRef, error, timer_err},
-					error_logger:error_msg("Timer couldn't be created for ~p drop(~p)", 
-										   [State#dmstate.machineid,Reason]),
-					loop({idle, State}, Parent, Debug)
+terminate (_Reason, _State) ->
+	ok.
+
+code_change (_OldVsn, State, _Extra) when is_tuple(State) ->
+	{ok, State}.
+
+handle_cast ({got_response, CommPid, Response}, {idle, State}) ->
+	case State#dmstate.commpid of
+		CommPid ->
+			case Response of
+				drop_ack ->
+					error_logger:error_msg("Drop Ack Received while not dropping!"),
+					{noreply, {idle, State}};
+				drop_nack ->
+					error_logger:error_msg("Drop Nack Received while not dropping!"),
+					{noreply, {idle, State}};
+				{temperature, _DateTime, Temperature} ->
+					io:format("Got temperature: ~p~n", [Temperature]),
+					{noreply, {idle, State#dmstate{temperature=Temperature}}};
+				Response ->
+					io:format("Unknown response: ~p~n", [Response]),
+					{noreply, {idle, State}}
 			end;
-		{system, From, Request} ->
-			sys:handle_system_msg(Request, From, Parent, ?MODULE, Debug, {idle, State})
+		_Else ->
+			{noreply, {idle, State}}
 	end;
+handle_cast ({got_response, CommPid, Response}, {dropping, State}) ->
+	case State#dmstate.commpid of
+		CommPid ->
+			case Response of
+				drop_ack ->
+					timer:cancel(State#dmstate.timerref),
+					gen_server:reply(State#dmstate.droppid, {ok}),
+					io:format("Got a drop ack~n"),
+					{noreply, {idle, State}};
+				drop_nack ->
+					timer:cancel(State#dmstate.timerref),
+					gen_server:reply(State#dmstate.droppid, {error, drop_nack}),
+					io:format("Got a drop nack~n"),
+					{noreply, {idle, State}}
+			end;
+		_Else ->
+			{noreply, {dropping, State}}
+	end;
+handle_cast (_Request, State) ->
+	{noreply, State}.
 
-loop ({dropping, DropPid, Timer, DropRef, State}, Parent, Debug) ->
-	#dmstate{commpid=CommPid} = State,
-	receive
-		{got_response, CommPid, drop_ack} ->
-			timer:cancel(Timer),
-			DropPid ! {drop_result, DropRef, ok},
-			io:format("Got drop ack~n"),
-			loop({idle, State}, Parent, Debug);
-		{got_response, CommPid, drop_nack} ->
-			timer:cancel(Timer),
-			DropPid ! {drop_result, DropRef, error, drop_nack},
-			error_logger:error_msg("Drop Nack received from ~p~n", [State#dmstate.machineid]),
-			loop({idle, State}, Parent, Debug);
-		{timeout, DropRef} ->
-			DropPid ! {drop_result, DropRef, error, timeout},
+handle_call ({drop, Slot}, {DropPid, DropRef}, {idle, State}) ->
+	case timer:send_after(timer:seconds(30), {timeout, DropRef}) of
+		{ok, Timer} ->
+			drink_machine_comm:send_command(State#dmstate.commpid, {drop, Slot}),
+			{noreply, {dropping, State#dmstate{timerref=Timer,dropref=DropRef,droppid={DropPid, DropRef}}}};
+		{error, Reason} ->
+			error_logger:error_msg("Timer couldn't be created for ~p drop(~p)",
+									[State#dmstate.machineid,Reason]),
+			{reply, {error, timer_err}, {idle, State}}
+	end;
+handle_call (_Request, _From, State) ->
+	{reply, {error, unknown}, State}.
+
+handle_info ({timeout, _DropRef},				{idle, State}) ->
+	{noreply, {idle, State}};
+handle_info ({timeout, DropRef},				{dropping, State}) ->
+	case State#dmstate.dropref of
+		DropRef ->
+			timer:cancel(State#dmstate.timerref),
 			error_logger:error_msg("Drop timed out on ~p~n", [State#dmstate.machineid]),
-			loop({idle, State}, Parent, Debug);
-		{system, From, Request} ->
-			sys:handle_system_msg(Request, From, Parent, ?MODULE, Debug,
-								  {dropping, DropPid, Timer, DropRef, State})
-	end.
-
-system_continue (Parent, Debug, State) ->
-	loop(State, Parent, Debug).
-
-system_terminate (Reason, _Parent, _Debug, _State) ->
-	exit(Reason).
+			gen_server:reply(State#dmstate.droppid, {error, timeout})
+	end,
+	{noreply, {idle, State}}.
 
 % Callback from Drink Machine Comm
 got_response (MachinePid, Response) ->
-	MachinePid ! {got_response, self(), Response}.
+	gen_server:cast(MachinePid, {got_response, self(), Response}).
 
 % API
 drop (MachinePid, Slot) when is_integer(Slot) ->
-	Ref = make_ref(),
-	MachinePid ! {api, drop, self(), Ref, Slot},
-	receive
-		{drop_result, Ref, ok} ->
-			ok;
-		{drop_result, Ref, error, Reason} ->
-			{error, Reason}
-	end.
+	gen_server:call(MachinePid, {drop, Slot}, infinity).

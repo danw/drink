7a966501f2cccfa6fcd3cb0dfb11a55a2e3b6f2e
Dan Willemsen <dan@csh.rit.edu>
2008-10-29 22:36:48 +0000

    whoops

--- drink_sup.erl
+++ drink_sup.erl
@@ -30,6 +30,6 @@ init ([]) ->
 		    permanent,			% Always restart
 		    100,				% Allow 100 seconds for it to shutdown
 		    worker,				% Not a supervisor
-		    [user_auth]},		% Uses the user_auth Module
+		    [user_auth]}		% Uses the user_auth Module
 		  ]
 		}}.

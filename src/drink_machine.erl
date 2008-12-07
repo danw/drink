0ffad20b45a9e058f90373bb0546f40510d9d7ab
Dan Willemsen <dan@csh.rit.edu>
2008-12-06 22:42:56 -0500

    makes more sense to have drink_machine:is_alive instead of drink_machines_sup:machine_is_alive - this also will help when non-connected machines can be talked to

--- src/drink_machine.erl
+++ src/drink_machine.erl
@@ -5,7 +5,7 @@
 -export ([init/1]).
 -export ([handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export ([got_response/2]).
--export ([slots/1, drop/2, temperature/1, slot_info/2]).
+-export ([slots/1, drop/2, temperature/1, slot_info/2, is_alive/1]).
 
 -include ("drink_mnesia.hrl").
 -include_lib ("stdlib/include/qlc.hrl").
@@ -180,6 +180,9 @@ slots (MachinePid) ->
 
 slot_info (MachinePid, Slot) when is_integer(Slot) ->
 	safe_gen_call(MachinePid, {slot_info, Slot}).
+
+is_alive (Machine) when is_atom(Machine) ->
+    drink_machines_sup:is_machine_alive(Machine).
 
 temperature (MachinePid) ->
 	safe_gen_call(MachinePid, {temp}).

e712e20d37d5c76ccd246c6ef50004c75916c797
Dan Willemsen <dan@danw.org>
2009-01-04 23:06:42 -0600

    webauth works!

--- src/webauth/authmod_webauth.erl
+++ src/webauth/authmod_webauth.erl
@@ -29,6 +29,8 @@
 -export ([start/1, stop/0, auth/2, out/1]).
 -export ([init/1, terminate/2, code_change/3, handle_call/3, handle_info/2, handle_cast/2]).
 
+-export ([token_attr_split/1, token_descape/1]).
+
 -include ("yaws.hrl").
 -include ("yaws_api.hrl").
 
@@ -97,7 +99,7 @@ init(Sconf) when is_record(Sconf, sconf) ->
     PortName = filename:join(code:priv_dir(drink), "ewebauth"),
     Port = open_port({spawn, PortName}, [{packet, 2}, binary, exit_status]),
     
-    ?WARNING("Starting authmod_webauth ... initialized~n"),
+    AppKey = #webauth_key{name = app, data = crypto:rand_bytes(16)},
     
     {ok, #was_state{
         port = Port,
@@ -105,7 +107,8 @@ init(Sconf) when is_record(Sconf, sconf) ->
         sslca = Sslca,
         login_url = LoginUrl,
         kdc_url = KdcUrl,
-        kdc_princ = KdcPrinc
+        kdc_princ = KdcPrinc,
+        keys = [AppKey]
     }}.
 
 terminate(_Reason, State) when is_record(State, was_state) ->
@@ -130,9 +133,11 @@ handle_call({auth, Arg, _Auth}, _From, State) ->
             {reply, {appmod, authmod_webauth}, State};
         CookieStr -> % Found cookie, check validity, insert user record
             case webauth_cookie_valid(CookieStr, State) of
-                {true, User} ->
+                {ok, User} ->
+                    io:format("got user: ~p~n", [User]),
                     {reply, {true, {User, undefined, undefined}}, State};
-                _ ->
+                Else ->
+                    error_logger:error_msg(":( bad cookie: ~p~n", [Else]),
                     {reply, {appmod, authmod_webauth}, State}
             end
     end;
@@ -143,8 +148,16 @@ handle_call({handle, Arg}, _From, State) ->
         ?WEBAUTH_RETURN_ARG ++ RetTokenPlus -> % Handle KDC response
             % Remove the trailing ;
             RetToken = lists:sublist(RetTokenPlus, length(RetTokenPlus) - 1),
-            {ok, Attrs, NewState} = webkdc_request_reply(RetToken, State),
-            {reply, {html, "Got RetToken: " ++ Attrs}, NewState};
+            case catch webkdc_request_reply(RetToken, State) of
+                {ok, {User, _, _} = Attrs, NewState} ->
+                    AppToken = webauth_cookie_create(Attrs, NewState),
+                    Cookie = yaws_api:setcookie(?WEBAUTH_COOKIE, AppToken, "/", "", "", on),
+                    {reply, [Cookie, {html, "Welcome, " ++ User}], NewState};
+                {error, Reason, NewState} ->
+                    {reply, [{html, "Permission Denied: " ++ io_lib:format("~p", [Reason])}], NewState};
+                E ->
+                    {reply, [{html, "Permission Denied: " ++ io_lib:format("~p", [E])}], State}
+            end;
         _ ->
             % TODO: strip webauth cookies?
             {ok, Url, NewState} = webkdc_request_url(Arg, State),
@@ -162,6 +175,17 @@ handle_info(_Request, State) ->
     {noreply, State}.
 
 % Private calls
+app_key(State) ->
+    AppKeys = lists:filter(fun(Key) ->
+            (Key#webauth_key.name =:= app)
+        end, State#was_state.keys),
+    case AppKeys of
+        [] ->
+            {error, no_app_key};
+        Keys ->
+            {ok, hd(Keys)}
+    end.
+
 session_key(State) ->
     Now = calendar:universal_time(),
     SessionKeys = lists:filter(fun(Key) -> 
@@ -237,7 +261,29 @@ request_key_expires(Data) ->
     end.
 
 webauth_cookie_valid(CookieStr, State) ->
-    {true, "Testing"}.
+    {ok, AppKey} = app_key(State),
+    case catch token_decode(CookieStr, AppKey) of
+        Attrs when is_list(Attrs) ->
+            case {lists:keysearch(t, 1, Attrs), lists:keysearch(et, 1, Attrs), lists:keysearch(s, 1, Attrs)} of
+                {{value, {t, "app"}}, {value, {et, ExpireTime}}, {value, {s, User}}} ->
+                    Now = calendar:universal_time(),
+                    if
+                        ExpireTime > Now ->
+                            {ok, User};
+                        true ->
+                            {error, expired}
+                    end;
+                Else ->
+                    {error, Else}
+            end;
+        Else ->
+            {error, Else}
+    end.
+
+webauth_cookie_create({User, Created, Expires}, State) ->
+    Attrs = [{t, app}, {et, Expires}, {ct, Created}, {s, User}],
+    {ok, AppKey} = app_key(State),
+    token_encode(Attrs, AppKey).
 
 webkdc_request_url(Arg, State) ->
     {ok, SessionKey, NewState} = session_key(State),
@@ -258,7 +304,16 @@ webkdc_request_token(Arg, SessionKey, State) ->
 
 webkdc_request_reply(Token, State) ->
     {ok, SessionKey, NewState} = session_key(State),
-    {ok, token_decode(Token, SessionKey), NewState}.
+    Attrs = token_decode(Token, SessionKey),
+    case {lists:keysearch(t, 1, Attrs), lists:keysearch(s, 1, Attrs), lists:keysearch(ct, 1, Attrs), lists:keysearch(et, 1, Attrs)} of
+        {{value, {t, "id"}},
+         {value, {s, User}},
+         {value, {ct, CreationTime}},
+         {value, {et, Expires}}} ->
+            {ok, {User, CreationTime, Expires}, NewState};
+        Else ->
+            {error, Else, NewState}
+    end.
 
 token_encode(Attrs, Key) ->
     Time = calendar:datetime_to_gregorian_seconds(calendar:universal_time()) - calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
@@ -284,7 +339,17 @@ token_escape(Bin) when is_binary(Bin) ->
     token_escape(binary_to_list(Bin));
 token_escape(Atom) when is_atom(Atom) ->
     token_escape(atom_to_list(Atom));
+token_escape({{_,_,_}, {_,_,_}} = Time) ->
+    DateTime = calendar:datetime_to_gregorian_seconds(Time) - calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
+    token_escape(<<DateTime:32/big>>);
 token_escape(Str) -> Str.
+
+token_descape([]) -> [];
+token_descape([$;, $;]) -> [$;];
+token_descape([$; | [$; | Rest]]) ->
+    [$;] ++ token_descape(Rest);
+token_descape([A | Rest]) ->
+    [A] ++ token_descape(Rest).
 
 token_padding(Bin) when is_binary(Bin) ->
     token_padding(size(Bin));
@@ -301,4 +366,33 @@ token_decode(<<Time:32/big, Encrypted/binary>>, Key) ->
     <<_:PaddingPos/binary, PaddingLen:8>> = Ending,
     AttrLen = size(Ending) - PaddingLen,
     <<Attrs:AttrLen/binary, _/binary>> = Ending,
-    binary_to_list(Attrs).
+    token_attr_split(binary_to_list(Attrs)).
+
+token_attr_split([]) -> [];
+token_attr_split(Attrs) ->
+    NameEnd = string:chr(Attrs, $=),
+    Name = list_to_atom(lists:sublist(Attrs, 1, NameEnd - 1)),
+    ValueEnd = token_attr_split_escaped(Attrs, NameEnd + 1),
+    Value = lists:sublist(Attrs, NameEnd + 1, ValueEnd - NameEnd - 2),
+    [{Name, token_value_format(Name, token_descape(Value))}] ++ token_attr_split(lists:sublist(Attrs, ValueEnd, length(Attrs))).
+
+token_attr_split_escaped(Str, StartAt) ->
+    List = lists:sublist(Str, StartAt, length(Str)),
+    Found = string:chr(List, $;),
+    if
+        length(List) >= (Found + 1) ->
+            case lists:nth(Found + 1, List) of
+                $; ->
+                    token_attr_split_escaped(Str, StartAt + Found + 1);
+                _ ->
+                    Found + StartAt
+            end;
+        true ->
+            Found + StartAt
+    end.
+
+token_value_format(ct, V) -> token_value_format(binary_date, list_to_binary(V));
+token_value_format(et, V) -> token_value_format(binary_date, list_to_binary(V));
+token_value_format(binary_date, <<Time:32/big>>) ->
+    calendar:gregorian_seconds_to_datetime(Time + calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}));
+token_value_format(_, Value) -> Value.
